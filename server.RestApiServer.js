d4b9bc31f7f75caaa46b876bcdd6db986c0ddcb6
Manuel Herrmann <it@icetruck.de>
2015-11-28 15:31:18 +0100

    (simple)restapi interface

---
+++ server.RestApiServer.js
@@ -0,0 +1,199 @@
+/* jshint asi: true */
+/* jshint esnext: true */
+
+
+var ChatClientBase = require('./server.ChatClientBase.js');
+var util = require("util");
+var express = require("express");
+var session = require('express-session');
+
+
+var tokenChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+function generateToken(clients) {
+	var token;
+	do {
+		token = '';
+		for (var i = 0; i < 32; ++i)
+			token += tokenChars[Math.floor(Math.random()*tokenChars.length)];
+	} while(clients[token] !== void 0);
+	return token;
+};
+
+function RestClient(chatServerBase, clients, req) {
+	this.clients = clients;
+	this.chatServerBase = chatServerBase;
+	this.updateTimeout(req);
+	this.updateIp(req, chatServerBase.config.x_forwarded_for);
+	this.token = req.session.id;
+	this.clients[this.token] = this;
+	this.storage = [];
+}
+util.inherits(RestClient, ChatClientBase);
+RestClient.prototype.login = function(nick, channel) {
+	this.nick = nick;
+	this.channel = channel;
+	/*var msg = {cmd:'join', channel: channel, nick: nick, pass: data.pass};
+	chatServerBase.onMessage(this, msg);*/
+};
+RestClient.prototype.getId = function() {
+	return this.ircClient;
+};
+RestClient.prototype.addToStorage = function(data) {
+	this.storage.push(data);
+};
+RestClient.prototype.applyStorage = function(list) {
+	while (this.storage.length > 0)
+		list.push(this.storage.shift());
+};
+RestClient.prototype.updateTimeout = function(req) {
+	var self = this;
+	if (this.timeout !== void 0)
+		clearTimeout(this.timeout);
+
+	// 6 mins timeout, clients should ping at least every 4 mins	
+	var nextTimeout = 6*60*1000;
+
+	this.timeout = setTimeout(function() { self.close(); }, nextTimeout);
+	req.session.cookie.expires = new Date(Date.now() + nextTimeout);
+	req.session.cookie.maxAge = nextTimeout;
+};
+RestClient.prototype.updateIp = function(req, bForwardingEnabled) {
+	if (bForwardingEnabled) {
+		this.ip = req.get('x-forwarded-for') || req.ip;
+	} else {
+		this.ip = req.ip;
+	}
+};
+RestClient.prototype.getIpAddress = function() {
+	return this.ip;
+};
+RestClient.prototype.close = function() {
+	clearTimeout(this.timeout);
+	this.chatServerBase.onClose(this);
+	delete this.clients[this.token];
+};
+RestClient.prototype.send = null; // is set before login
+
+
+function RestApiServer() {
+}
+module.exports = function() {
+	return new RestApiServer();
+};
+RestApiServer.prototype.initialize = function(config) {
+	this.config = config;
+};
+RestApiServer.prototype.run = function(chatServerBase) {
+	this.chatServerBase = chatServerBase;
+	var self = this;
+
+	var app = express();
+	var clients = {}; // token to client
+
+	var secret = '';
+	for(var i = 0; i < 32; ++i)
+		secret += String.fromCharCode(Math.floor(Math.random()*26));
+
+	app.use(session({
+		genid: function(req) {
+			return generateToken(clients) // use UUIDs for session IDs 
+		},
+		secret: secret,
+		resave: true,
+		saveUninitialized: true
+	}));
+
+	app.get('/', function(req, res) {
+		res.send('');
+	});
+	app.get('/login', function(req, res) {
+		var client = clients[req.session.id] = new RestClient(chatServerBase, clients, req);
+		client.updateTimeout(req);
+		client.updateIp(req, self.config.x_forwarded_for);
+		client.send = function(data) {
+			client.addToStorage(data);
+		};
+		res.send(JSON.stringify({results:[{login:'ok'}]}));
+	});
+	app.get('/logout', function(req, res) {
+		var client = clients[req.session.id];
+		if (client === void 0) {
+			res.send('');
+			return;
+		}
+		client.updateTimeout(req);
+		client.updateIp(req, self.config.x_forwarded_for);
+
+		var outData = {
+			results: [
+			]
+		};
+		client.applyStorage(outData.results);
+		res.send(JSON.stringify(outData));
+		client.close();
+	});
+	app.get('/ping', function(req, res) { // required at least 5 mins after last poll
+		var client = clients[req.session.id];
+		if (client === void 0) {
+			res.send('');
+			return;
+		}
+		client.updateTimeout(req);
+		client.updateIp(req, self.config.x_forwarded_for);
+
+		var outData = {
+			results: [
+				{cmd:'pong'}
+			]
+		};
+		// dont move store on pings
+		res.send(JSON.stringify(outData));
+	});
+	app.get('/poll', function(req, res) {
+		var client = clients[req.session.id];
+		if (client === void 0) {
+			res.send('');
+			return;
+		}
+		
+		client.updateTimeout(req);
+		client.updateIp(req, self.config.x_forwarded_for);
+
+		var outData = {
+			results: [
+			]
+		};
+		client.applyStorage(outData.results);
+		res.send(JSON.stringify(outData));
+	});
+	app.post('/send', function(req, res) {
+		var client = clients[req.session.id];
+		if (client === void 0) {
+			res.send('');
+			return;
+		}
+
+		var content = [];
+		req.on('data', function (data) {
+			content.push(data);
+		});
+		req.on('end', function() {
+			var data = JSON.parse(content.join(''));
+
+			chatServerBase.onMessage(client, data);
+
+			client.updateTimeout(req);
+			client.updateIp(req, self.config.x_forwarded_for);
+
+			var outData = {
+				results: [
+				]
+			};
+			client.applyStorage(outData.results);
+			res.send(JSON.stringify(outData));
+		});
+	});
+	
+	app.listen(this.config.port, this.config.ip);
+	console.log("Started rest api server on " + this.config.ip + ":" + this.config.port);
+};
--- server.js
+++ server.js
@@ -18,16 +18,20 @@ function main() {
 	var chatServerBase = require("./server.ChatServerBase.js")();
 	chatServerBase.initialize(config.base, version);
 
-	if (config.webSocketServer.enabled) {
+	if (config.webSocketServer && config.webSocketServer.enabled) {
 		var webSocketServer = require("./server.WebSocketServer.js")();
 		webSocketServer.initialize(config.webSocketServer);
 		webSocketServer.run(chatServerBase);
 	}
-
-	if (config.ircServer.enabled) {
+	if (config.ircServer && config.ircServer.enabled) {
 		var ircServer = require("./server.IrcServer.js")();
 		ircServer.initialize(config.ircServer);
 		ircServer.run(chatServerBase);
+	}
+	if (config.restApiServer && config.restApiServer.enabled) {
+		var restApiServer = require("./server.RestApiServer.js")();
+		restApiServer.initialize(config.restApiServer);
+		restApiServer.run(chatServerBase);
 	}
 
 	fs.watchFile(configFileName, {persistent: false}, function() {
@@ -40,8 +44,9 @@ function main() {
                 version = loadFile(versionFileName).match(/"([0-9]+)"/)[1];
 		console.log("Running version " + version);
                 chatServerBase.initialize(config.base, version);
-                if (config.webSocketServer.enabled) webSocketServer.initialize(config.webSocketServer);
-                if (config.ircServer.enabled) ircServer.initialize(config.ircServer);
+                if (config.webSocketServer && config.webSocketServer.enabled) webSocketServer.initialize(config.webSocketServer);
+                if (config.ircServer && config.ircServer.enabled) ircServer.initialize(config.ircServer);
+                if (config.restApiServer && config.restApiServer.enabled) ircServer.initialize(config.ircServer);
         });
 }
 
