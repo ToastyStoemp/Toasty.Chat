124a434bceffd69325865b6b4c7860a27002ee3e
ToastyStoemp <foreverwolf@live.be>
2015-12-16 04:34:49 +0100

    Added Donate command + listing

--- client/data.js
+++ client/data.js
@@ -1,3 +1,9 @@
 var webClientVersion = "201512160013";
 var tripPost = {"kFfbGV":"00010111", "fen7JT":"3600"};
 var donatorList = ["Shrooms"];
+
+module.exports = {
+  webClientVersion: webClientVersion,
+  tripPost: tripPost,
+  donatorList: donatorList
+};
---
+++ commands/donate.js
@@ -0,0 +1,19 @@
+var data = require("../client/data");
+
+var donate = function(bot, sender, args)
+{
+	try
+	{
+		var text = "Feel free to make a donation to this BTC-wallet:\n1L4wsdovW42KYNfQ1UE7jyZJPk4XiJJTHS\n\nList of previous donators:\n"
+		for (var i in data.donatorList) {
+			text += "*" + data.donatorList[i] + "\n";
+		}
+		bot.send(text);
+	}
+	catch(e)
+	{
+		bot.send(e.toString());
+	}
+};
+
+module.exports = {donate: donate};
