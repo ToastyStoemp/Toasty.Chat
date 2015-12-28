bd1bc116c1ff4a3382ca0e15189c107d3e9a0898
ToastyStoemp <foreverwolf@live.be>
2015-12-28 17:10:28 +0100

    Fixed urban command

--- commands/urban.js
+++ commands/urban.js
@@ -4,28 +4,28 @@ exports.urban = function(bot, sender, args, data, client)
 {
     if(args.join(" ").trim() == "")
     {
-        bot.send("Usage: !urban <term>");
-        return;
+      bot.sendClient("Usage: !urban <term>", client);
+      return;
     }
 
     var term = encodeURIComponent(args.join(" "));
 
     request("http://api.urbandictionary.com/v0/define?term=" + term, function(err, res, body)
     {
-        if(err)
+      if(err)
     	{
-            bot.sendClient("@" + sender + " " + err.toString(), client);
-            return;
-        }
+          bot.sendClient("@" + sender + " " + err.toString(), client);
+          return;
+      }
 
-        var data = JSON.parse(body);
-        if(!data.list || data.list.length < 1)
-        {
-            bot.sendClient("@" + sender + " no results found", client);
-            return;
-        }
+      var data = JSON.parse(body);
+      if(!data.list || data.list.length < 1)
+      {
+          bot.sendClient("@" + sender + " no results found", client);
+          return;
+      }
 
-        var defintion = data.list[0].definition.substr(0, 500);
-        bot.sendAll("@" + sender + " " + defintion + " - " + data.list[0].permalink, client);
+      var defintion = data.list[0].definition.substr(0, 500);
+      bot.sendAll("@" + sender + " " + defintion + " - " + data.list[0].permalink, client);
     });
 };
