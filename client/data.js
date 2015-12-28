a62d729f650b23061a1201826ab615d2ee9ca8a5
wolf <foreverwolf@livebe>
2015-12-28 17:20:19 +0100

    update

--- client/data.js
+++ client/data.js
@@ -1,7 +1,7 @@
 var webClientVersion = "201512160013";
 var tripPost = {"kFfbGV":"00010111", "fen7JT":"3600"};
-var donatorList = ["SoN/TXpyl4"]
-var donatorNames = ["Shrooms"];
+var donatorList = ["SoN/TXpyl4", "gA2a6wGjOO"]
+var donatorNames = ["Shrooms", "M$GNV%"];
 
 module.exports = {
   webClientVersion: webClientVersion,
