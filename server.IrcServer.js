33ec289856789a66d3aa6c6bd1c8cb1474e75c09
Manuel Herrmann <git@0x17.de>
2016-03-04 23:27:37 +0100

    fixed close call for irc users

--- server.IrcServer.js
+++ server.IrcServer.js
@@ -32,6 +32,7 @@ ToastyClient.prototype.getIpAddress = function() {
 }
 ToastyClient.prototype.close = function() {
 	this.ircClient.chatServerBase.onClose(this);
+	this.ircClient.close();
 }
 ToastyClient.prototype._generateIdentifier = function(nick) {
 	return nick+'!~'+nick+'@'+this.ircClient.config.serverHostname;
