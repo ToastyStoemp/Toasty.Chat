d5cb50d2a3252f3a923cf7bda711aeba58be308f
Rafaël Naciri <rafael.naciri.dev@gmail.com>
2016-07-30 23:40:28 +0200

    Instant reconnection if unwarranted disconnect

--- server.Bouncer.js
+++ server.Bouncer.js
@@ -35,13 +35,123 @@ BouncerClient.prototype.send = function (causingClient, data) {
     }
 };
 
-
 function Bouncer() {
     this.relays = {};
 }
 module.exports = function () {
     return new Bouncer();
 };
+
+Bouncer.prototype.openRelay = function (relayInfo) {
+    var that = this;
+    var relay = new ws(this.config.wss);
+    relay.sockets = {};
+    relay.on("open", function () {
+        this.nick = relayInfo.nick;
+        this.pass = relayInfo.pass;
+        this.channel = relayInfo.channel;
+        this.send(JSON.stringify({"cmd": "join", "nick": this.nick, "pass": this.pass, "channel": this.channel}));
+    });
+    relay.on("message", function (data) {
+        var args = JSON.parse(data);
+        console.log("Relay", args);
+        switch (args.cmd) {
+            case "warn":
+                if (args.errCode === "E005" || args.errCode === "E003") {
+                    socket.nick = null;
+                }
+                break;
+            case "onlineSet":
+                for (var socketId in this.sockets) {
+                    if (this.sockets.hasOwnProperty(socketId)) {
+                        this.sockets[socketId].send(data);
+                    }
+                }
+                this.memory = [];
+                this.nicks = args.nicks;
+                this.trips = args.trips;
+                return;
+                break;
+            case "onlineAdd":
+                this.nicks.push(args.nick);
+                this.trips.push(args.trip);
+                break;
+            case "onlineRemove":
+                var idx = this.nicks.indexOf(args.nick);
+                this.nicks.splice(idx, 1);
+                this.trips.splice(idx, 1);
+                break;
+            case "ping":
+                for (var socketId in this.sockets) {
+                    if (this.sockets.hasOwnProperty(socketId)) {
+                        socket.send(JSON.stringify({cmd: "pong"}));
+                    }
+                }
+                break;
+        }
+        if (this.hasOwnProperty("sockets") && this.sockets !== null && !this.sockets.isEmpty()) {
+            for (var socketId in this.sockets) {
+                if (this.sockets.hasOwnProperty(socketId)) {
+                    var isOpened = false;
+                    while (!isOpened) {
+                        try {
+                            this.sockets[socketId].send(data);
+                            isOpened = true;
+                        } catch (e) {
+                            console.error(e);
+                        }
+                    }
+                }
+            }
+        } else {
+            if (this.memory != null) {
+                if (this.memory.length > that.config.memorySize) {
+                    var idx = 0;
+                    if (that.config.mentionHold) {
+                        for (; idx < this.memory.length && this.memory[idx].mention; idx++);
+                    }
+                    this.memory.splice(idx, 1);
+                }
+                else {
+                    this.memory = [];
+                }
+            }
+            var reg = new RegExp("@" + relayInfo.nick + "(\\s|$)");
+            var mention = reg.test(data);
+            this.memory.push({"data": data, "mention": mention});
+        }
+    });
+    relay.on("close", function () {
+        console.log("Relay close");
+        if (this.sockets !== null) {
+            while (true) {
+                try {
+                    that.relays[this.nick][this.pass][this.channel] = that.openRelay({
+                        "nick": this.nick,
+                        "pass": this.pass,
+                        "channel": this.channel
+                    });
+                    return;
+                } catch (e) {
+                    console.error(e);
+                }
+            }
+        }
+        try {
+            delete that.relays[this.nick][this.pass][this.channel];
+            if (that.relays[this.nick][this.pass].isEmpty()) {
+                delete that.relays[this.nick][this.pass];
+                if (that.relays[this.nick].isEmpty()) {
+                    delete that.relays[this.nick];
+                }
+            }
+        } catch (e) {
+
+        }
+    });
+    return relay;
+};
+
 Bouncer.prototype.initialize = function (config) {
     this.config = config;
 };
@@ -74,96 +184,10 @@ Bouncer.prototype.run = function () {
                         relay.memory = [];
                         this.send(JSON.stringify({"cmd": "onlineSet", "nicks": relay.nicks, "trips": relay.trips}));
                     } else {
-                        relay = new ws(that.config.wss);
-                        relay.sockets = {};
+                        relay = that.openRelay(socket);
                         that.relays[this.nick] = {};
                         that.relays[this.nick][this.pass] = {};
                         that.relays[this.nick][this.pass][this.channel] = relay;
-                        relay.on("open", function () {
-                            this.nick = socket.nick;
-                            this.pass = socket.pass;
-                            this.channel = socket.channel;
-                            this.send(data);
-                        });
-                        relay.on("message", function (data) {
-                            var args = JSON.parse(data);
-                            console.log("Relay", args);
-                            switch (args.cmd) {
-                                case "warn":
-                                    if (args.errCode === "E005" || args.errCode === "E003") {
-                                        socket.nick = null;
-                                    }
-                                    break;
-                                case "onlineSet":
-                                    for (var socketId in this.sockets) {
-                                        if (this.sockets.hasOwnProperty(socketId)) {
-                                            this.sockets[socketId].send(data);
-                                        }
-                                    }
-                                    this.memory = [];
-                                    this.nicks = args.nicks;
-                                    this.trips = args.trips;
-                                    return;
-                                    break;
-                                case "onlineAdd":
-                                    this.nicks.push(args.nick);
-                                    this.trips.push(args.trip);
-                                    break;
-                                case "onlineRemove":
-                                    var idx = this.nicks.indexOf(args.nick);
-                                    this.nicks.splice(idx, 1);
-                                    this.trips.splice(idx, 1);
-                                    break;
-                                case "ping":
-                                    for (var socketId in this.sockets) {
-                                        if (this.sockets.hasOwnProperty(socketId)) {
-                                            socket.send(JSON.stringify({cmd: "pong"}));
-                                        }
-                                    }
-                                    break;
-                            }
-                            if (this.hasOwnProperty("sockets") && this.sockets !== null && !this.sockets.isEmpty()) {
-                                for (var socketId in this.sockets) {
-                                    if (this.sockets.hasOwnProperty(socketId)) {
-                                        var isOpened = false;
-                                        while (!isOpened) {
-                                            try {
-                                                this.sockets[socketId].send(data);
-                                                isOpened = true;
-                                            } catch (e) {
-                                                console.error(e);
-                                            }
-                                        }
-                                    }
-                                }
-                            } else {
-                                if (this.memory.length > that.config.memorySize) {
-                                    var idx = 0;
-                                    if (that.config.mentionHold) {
-                                        for (; idx < this.memory.length && this.memory[idx].mention; idx++);
-                                    }
-                                    this.memory.splice(idx, 1);
-                                }
-                                var reg = new RegExp("@" + socket.nick + "(\\s|$)");
-                                var mention = reg.test(data);
-                                this.memory.push({"data": data, "mention": mention});
-                            }
-                        });
-                        relay.on("close", function () {
-                            console.log("Relay close");
-                            try {
-                                delete that.relays[this.nick][this.pass][this.channel];
-                                if (that.relays[this.nick][this.pass].isEmpty()) {
-                                    delete that.relays[this.nick][this.pass];
-                                    if (that.relays[this.nick].isEmpty()) {
-                                        delete that.relays[this.nick];
-                                    }
-                                }
-
-                            } catch (e) {
-
-                            }
-                        });
                     }
                     this.socketId = new Date().getTime();
                     relay.sockets[this.socketId] = this;
@@ -177,9 +201,11 @@ Bouncer.prototype.run = function () {
                             case "quit":
                                 for (var socketId in that.relays[this.nick][this.pass][this.channel].sockets) {
                                     if (that.relays[this.nick][this.pass][this.channel].sockets.hasOwnProperty(socketId)) {
-                                        socket.close();
+                                        that.relays[this.nick][this.pass][this.channel].sockets[socketId].close();
+                                        delete that.relays[this.nick][this.pass][this.channel].sockets[socketId];
                                     }
                                 }
+                                that.relays[this.nick][this.pass][this.channel].sockets = null;
                                 that.relays[this.nick][this.pass][this.channel].close();
                                 delete that.relays[this.nick][this.pass][this.channel];
                                 if (that.relays[this.nick][this.pass].isEmpty()) {
@@ -201,7 +227,11 @@ Bouncer.prototype.run = function () {
                     return;
                     break;
             }
-            that.relays[this.nick][this.pass][this.channel].send(data);
+            try {
+                that.relays[this.nick][this.pass][this.channel].send(data);
+            } catch (e) {
+
+            }
         });
         socket.on('close', function () {
             console.log("Socket close");
