cee6af8652bb847faa4df65d9913ec3b63aaf5e2
Wolf Van Herreweghe <foreverwolf@live.be>
2016-08-12 15:12:07 +0200

    Updated whisper command

--- commands/whisper.js
+++ commands/whisper.js
@@ -1,11 +1,16 @@
 var whipser = function(bot, sender, args, data, client)
 {
-  var targetNick = args.splice(0,1);
+  if (args.length < 2) {
+    bot.sendClient("Usage is .w [nick] [message]", client);
+    return;
+  }
+  var targetNick = args.splice(0,1)[0];
   targetNick = targetNick.replace("@", "");
   var friend = bot.chatServerBase.getClientOfChannel(targetNick, client.channel) || null;
-  if (friend != null) {
+  if (friend != null)
     bot.sendClient(sender + " whispers: " + args.join(' '), friend);
-  }
+  else
+    bot.sendClient("User can not be found.", client);
 };
 
 module.exports = {w: whipser};
