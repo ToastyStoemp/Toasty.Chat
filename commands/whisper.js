eeac5935ad34bb4fae9123d5de0cacd2bb7c08ba
Rafaël Naciri <rafael.naciri.dev@gmail.com>
2016-08-15 03:22:51 +0200

    Display sent whispers

--- commands/whisper.js
+++ commands/whisper.js
@@ -1,16 +1,17 @@
-var whipser = function(bot, sender, args, data, client)
-{
-  if (args.length < 2) {
-    bot.sendClient("Usage is .w [nick] [message]", client);
-    return;
-  }
-  var targetNick = args.splice(0,1)[0];
-  targetNick = targetNick.replace("@", "");
-  var friend = bot.chatServerBase.getClientOfChannel(targetNick, client.channel) || null;
-  if (friend != null)
-    bot.sendClient(sender + " whispers: " + args.join(' '), friend);
-  else
-    bot.sendClient("User can not be found.", client);
+var whipser = function (bot, sender, args, data, client) {
+    if (args.length < 2) {
+        bot.sendClient("Usage is .w [nick] [message]", client);
+        return;
+    }
+    var targetNick = args.splice(0, 1)[0];
+    targetNick = targetNick.replace("@", "");
+    var friend = bot.chatServerBase.getClientOfChannel(targetNick, client.channel) || null;
+    if (friend != null) {
+        bot.sendClient("You whispered to " + targetNick + " : " + args.join(" "), client);
+        bot.sendClient(sender + " whispers: " + args.join(' '), friend);
+    }
+    else
+        bot.sendClient("User cannot be found.", client);
 };
 
 module.exports = {w: whipser};
