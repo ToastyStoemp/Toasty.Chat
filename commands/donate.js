aac11243e6740074260fd477dbc315e4ab9592e6
ToastyStoemp <foreverwolf@live.be>
2017-01-26 21:16:44 -0500

    added save to add donate and add trip

--- commands/donate.js
+++ commands/donate.js
@@ -1,51 +1,53 @@
+var fs = require("fs");
 var donatorList = require("../data/donators.json");
 
 var donate = function(bot, sender, args, data, client) {
-	var text =
-		"Feel free to make a donation here:\nhttps://donations.toastystoemp.com/\n\nList of previous donators:\n"
-	for (var i in donatorList) {
-		text += "*" + donatorList[i] + "\n";
-	}
-	bot.sendAll(text, client);
+    var text =
+        "Feel free to make a donation here:\nhttps://donations.toastystoemp.com/\n\nList of previous donators:\n"
+    for (var i in donatorList) {
+        text += "*" + donatorList[i] + "\n";
+    }
+    bot.sendAll(text, client);
 };
 
 var donateadd = function(bot, sender, args, data, client) {
-	if (args[0] == "") {
-		bot.sendClient("Usage: !adddonator [nick]", client);
-		return
-	}
-	for (var i in donatorList)
-		if (donatorList[i] == args[0]) {
-			bot.sendClient("Already on the list", client);
-			return;
-		}
+    if (args[0] == "") {
+        bot.sendClient("Usage: !adddonator [nick]", client);
+        return
+    }
+    for (var i in donatorList)
+        if (donatorList[i] == args[0]) {
+            bot.sendClient("Already on the list", client);
+            return;
+        }
 
-	donatorList[bot.commands['tripget'](bot, data.nick, args, data, client)] =
-		args[0];
-	bot.sendAll(args[0] +
-		" has been added to the list of donators! Thank you for your donation",
-		client);
-};
+    donatorList[bot.commands['tripget'](bot, data.nick, args, data, client)] =
+        args[0];
+    bot.sendAll(args[0] +
+        " has been added to the list of donators! Thank you for your donation",
+        client);
+
+    fs.writeFile("../data/donators.json", donatorList, function() {});
+}
 
 var donateremove = function(bot, sender, args, data, client) {
-	if (args[0] == "") {
-		bot.sendClient("Usage: !removedonator [nick]", client);
-		return
-	}
-	for (var i in donatorList)
-		if (donatorList[i] == args[0]) {
-			delete donatorList[bot.commands['tripget'](bot, data.nick, args, data,
-				client)];
-			bot.sendAll(args[0] + " has been removed from the list of donators!",
-				client);
-			return;
-		}
+    if (args[0] == "")
+        bot.sendClient("Usage: !removedonator [nick]", client);
+    else {
+        for (var i in donatorList)
+            if (donatorList[i] == args[0]) {
+                delete donatorList[bot.commands['tripget'](bot, data.nick, args, data, client)];
+                bot.sendAll(args[0] + " has been removed from the list of donators!", client);
+                fs.writeFile("../data/donators.json", donatorList, function() {});
+                return;
+            }
+    }
 
-	bot.sendClient("User not found", client);
+    bot.sendClient("User not found", client);
 };
 
 module.exports = {
-	donate: donate,
-	donateadd: donateadd,
-	donateremove: donateremove
+    donate: donate,
+    donateadd: donateadd,
+    donateremove: donateremove
 };
--- commands/trips.js
+++ commands/trips.js
@@ -1,47 +1,46 @@
+var fs = require("fs");
 var tripList = require("../data/trips.json");
 
-var tripget = function(bot, sender, args, data, client)
-{
-	if (args[0] == '')
-	{
-		bot.sendClient("Usage: '!get trip [nick]'", client);
-		return;
-	}
+var tripget = function(bot, sender, args, data, client) {
+    if (args[0] == '') {
+        bot.sendClient("Usage: '!get trip [nick]'", client);
+        return;
+    }
 
-	var target = bot.chatServerBase.getClientOfChannel(args[0], client.channel);
-	if (target == null) {
-		bot.sendClient("User not found.", client);
-		return;
-	}
+    var target = bot.chatServerBase.getClientOfChannel(args[0], client.channel);
+    if (target == null) {
+        bot.sendClient("User not found.", client);
+        return;
+    }
 
-	bot.sendClient(target.trip, client);
-	return target.trip;
+    bot.sendClient(target.trip, client);
+    return target.trip;
 };
 
-var tripadd = function(bot, sender, args, data, client)
-{
-	if (args[1] == "") {
-		bot.sendClient("Usage: !tripadd [nick] [trip]", client);
-		return
-	}
+var tripadd = function(bot, sender, args, data, client) {
+    if (args[1] == "") {
+        bot.sendClient("Usage: !tripadd [nick] [trip]", client);
+        return
+    }
 
-	tripList[bot.commands['tripget'](bot, data.nick, args, data, client)] = args[1];
-	bot.sendClient(args[0] + " has been added to the triplist.", client);
+    tripList[bot.commands['tripget'](bot, data.nick, args, data, client)] = args[1];
+    bot.sendClient(args[0] + " has been added to the triplist.", client);
+    fs.writeFile("../data/trips.json", tripList, function() {});
 };
 
-var tripremove = function(bot, sender, args, data, client)
-{
-	if (args[0] == "") {
-		bot.sendClient("Usage: !tripremove [nick]", client);
-		return
-	}
-	var realtrip = bot.commands['tripget'](bot, data.nick, args, data, client);
-	delete tripList[realtrip];
-	bot.sendAll(args[0] + " has been removed from the triplist!", client);
+var tripremove = function(bot, sender, args, data, client) {
+    if (args[0] == "") {
+        bot.sendClient("Usage: !tripremove [nick]", client);
+        return
+    }
+    var realtrip = bot.commands['tripget'](bot, data.nick, args, data, client);
+    delete tripList[realtrip];
+    bot.sendAll(args[0] + " has been removed from the triplist!", client);
+    fs.writeFile("../data/trips.json", tripList, function() {});
 };
 
 module.exports = {
-	tripget: tripget,
-	tripadd: tripadd,
-	tripremove: tripremove
+    tripget: tripget,
+    tripadd: tripadd,
+    tripremove: tripremove
 };
